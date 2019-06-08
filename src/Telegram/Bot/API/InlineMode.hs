8125540eeba404ef49f62df54ce838f2823516b1
Nicolas Lenz <nicolas@eisfunke.com>
2019-06-08 21:26:52 +0200

    Add first inline BotM functions

--- src/Telegram/Bot/API/InlineMode.hs
+++ src/Telegram/Bot/API/InlineMode.hs
@@ -1,1 +1,20 @@
 module Telegram.Bot.API.InlineMode where
+
+import           Control.Monad.Reader
+
+import           Telegram.Bot.API        as Telegram
+import           Telegram.Bot.Simple.Eff
+
+currentInlineQuery :: BotM (Maybe InlineQuery)
+currentInlineQuery = do
+  mupdate <- asks botContextUpdate
+  pure $ updateInlineQuery =<< mupdate
+
+answerInlineQuery :: [InlineQueryResult] -> BotM ()
+answerInlineQuery results = do
+  mqueryToAnswer <- currentInlineQuery
+  case mqueryToAnswer of
+      Just queryToAnswer -> do
+        let req = AnswerInlineQueryRequest (inlineQueryId queryToAnswer) results
+        void $ liftClientM $ Telegram.answerInlineQuery req
+      Nothing -> liftIO $ putStrLn "No inline query to answer"
